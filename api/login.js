ee970bd95e1a9ab322cdaebd5a7579f0ce161cbf
polygotsacademy-cmd <polygotsacademy@gmail.com>
2026-08-09 19:42:45 +0300

    Update login.js

--- api/login.js
+++ api/login.js
@@ -81,6 +81,8 @@ export default function handler(req, res) {
         { username: 'malik_w_a', password: '063972', payment_status: 'Paid' },
         // ادم محمد محمود
         { username: 'adam_m_m', password: '524817', payment_status: 'Paid' },
+        // يحيى محمد رمضان
+        { username: 'yaya_m_r', password: '520017', payment_status: 'Paid' },
         // فراو فريدة
         { username: 'frau_farida', password: '13502009', payment_status: 'Paid' }, 
     ];
